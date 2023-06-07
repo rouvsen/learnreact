49873bf7495f627509b99609249577a8f1eec7bf
rovshan <rouvsen@gmail.com>
2023-06-07 21:30:27 +0400

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useMemo, useRef, useState } from "react";
 // import Name from "./components/Name.jsx";
 // import Surname from "./components/Surname.jsx";
 
@@ -51,17 +51,51 @@ function App() {
   //   )
   // }
 
-  const [status, setStatus] = useState('');
+  // const [status, setStatus] = useState('');
 
-  useEffect(() => {
-    setStatus('Hello');
-  }, [])
+  // useEffect(() => {
+  //   setStatus('Hello');
+  // }, [])
 
-  return (
-    <div>
-      {status}
-    </div>
-  )
+  // const firstRef = useRef(0);
+
+  // console.log("firstRef", firstRef.current.value = 5);
+
+  // const refFunc = () => {
+  //   //firstRef.current
+  // }
+
+  // return (
+  //   <>
+  //   <div>{status}</div>
+  //   <input ref={firstRef} onClick={refFunc} placeholder="search"/>
+  //   </>
+  // )
+
+  // const [count, setCount] = useState(0);//default 0
+  // const [text1, setText1] = useState("");//default ""
+
+  // const func = (num) => {
+  //   console.log("calculating...");//console
+  //   for (let i = 0; i < 1_000_000; i++) {
+  //     num += 1;//num++
+  //   }
+  //   return num;//1_000_000
+  // }
+
+  // const memo = useMemo(() => func(count), [count]);//second param it means follow count
+  // //memo saving 1_000_000 in memory and don't render, until you change the value of count
+
+  // return (
+  //   <>
+  //   {memo}
+  //   <br></br>
+  //   <input value={text1} onChange={e => setText1(e.target.value)} placeholder="search"/>
+  //   </>
+  // )
+  // useMemo - one of the react hooks, greate choice in terms of time/memory/speed, memo is powerfull
+
+  
   
 }
 
